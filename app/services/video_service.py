9dae00d5ceeade093dce6d32b0e3a0e49e8d7801
kritika-ghosh <kri.kritika.ghosh@gmail.com>
2026-05-20 05:53:39 +0530

    fixed aspect ratio issues

--- app/services/video_service.py
+++ app/services/video_service.py
@@ -1,7 +1,7 @@
 import os
 import json
 # V2 Import (No '.editor'!)
-from moviepy import ImageClip, TextClip, CompositeVideoClip, concatenate_videoclips
+from moviepy import ImageClip, TextClip, CompositeVideoClip, concatenate_videoclips, ColorClip
 import platform
 if platform.system() == "Windows":
     os.environ["IMAGEMAGICK_BINARY"] = r"C:\Program Files\ImageMagick-7.1.2-Q16-HDRI\magick.exe"
@@ -39,20 +39,66 @@ class VideoService:
                 if not img_path or not os.path.exists(img_path):
                     continue
 
-                # V2 SYNTAX: with_duration() and resized()
-                base_clip = ImageClip(img_path).with_duration(duration).resized(height=1080, width=1920)
+                # Proportionally scale the image to fit 1920x1080 canvas
+                img_clip = ImageClip(img_path).with_duration(duration)
+                canvas_w, canvas_h = 1920, 1080
+                orig_w, orig_h = img_clip.size
+                
+                orig_aspect = orig_w / orig_h
+                canvas_aspect = canvas_w / canvas_h
+                
+                if orig_aspect > canvas_aspect:
+                    # Scale to fit width (letterbox)
+                    new_w = canvas_w
+                    new_h = int(canvas_w / orig_aspect)
+                else:
+                    # Scale to fit height (pillarbox)
+                    new_h = canvas_h
+                    new_w = int(canvas_h * orig_aspect)
+                
+                # Force dimensions to be even numbers to ensure codec compliance
+                new_w = (new_w // 2) * 2
+                new_h = (new_h // 2) * 2
+                
+                scaled_img = img_clip.resized(width=new_w, height=new_h)
+                
+                # Compose centered scaled image on a standard black background
+                bg_clip = ColorClip(size=(canvas_w, canvas_h), color=(0, 0, 0)).with_duration(duration)
+                base_clip = CompositeVideoClip(
+                    [bg_clip, scaled_img.with_position("center")],
+                    size=(canvas_w, canvas_h)
+                )
+
                 try:
+                    # Constrain the caption width to the visible width of the scaled image
+                    text_w = min(1600, int(new_w * 0.9))
+                    text_w = max(500, text_w)
+                    
+                    # Dynamically step down font size for narrower bounds
+                    font_size = 64
+                    if text_w < 1000:
+                        font_size = 48
+                    if text_w < 600:
+                        font_size = 38
+
                     txt_clip = TextClip(
                         text=caption, 
                         font=font_path, 
-                        font_size=64, 
+                        font_size=font_size, 
                         color='white', 
+                        stroke_color='black',
+                        stroke_width=2,
                         method='caption',
-                        size=(1600, None)
+                        size=(text_w, None)
                     )
                     
-                    txt_clip = txt_clip.with_position(('center', 880)).with_duration(duration)
-                    composite_scene = CompositeVideoClip([base_clip, txt_clip])
+                    # Align dynamically from the bottom based on rendered height
+                    margin_bottom = 60
+                    y_pos = canvas_h - txt_clip.h - margin_bottom
+                    y_pos = max(20, y_pos)  # Don't let it overflow above top
+
+                    txt_clip = txt_clip.with_position(('center', y_pos)).with_duration(duration)
+                    composite_scene = CompositeVideoClip([base_clip, txt_clip], size=(canvas_w, canvas_h))
                     compiled_clips.append(composite_scene)
                 
                 except Exception as e:
